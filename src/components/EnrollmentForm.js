49d19a86d853ed577c0dc611fd2ad2274b5d66cb
Lea Sae Cheul <dltpcjf1005@naver.com>
2023-03-03 09:59:58 +0900

    주석 추가

--- src/components/EnrollmentForm.js
+++ src/components/EnrollmentForm.js
@@ -4,16 +4,21 @@ import '../App.css';
 
 
 const EnrollmentForm = ()=>{
+    //폼에 입력한 내용을 기억하기 위해 state형 변수 선언
+    //onBlur 이벤트 발생 시 입력한 이름/성 을 firstName , lastName 변수에 저장
     const [firstName,setFirstName] =useState("");
     const [lastName,setLastName] =useState("");
+
+    //state형 변수에 저장된 이름/성을 환영메세지로 출력하기 위해 선언
     const [welcomeMessage,setWelcomeMessage] =useState("");
 
+    //등록하기 버튼 클릭 시 이름/성을 환영 메세지로 만들어 폼 아래쪽에 나타냄
     const handleSubmit = (e) => {
         setWelcomeMessage(`환영합니다. ${firstName} ${lastName} 님!!` );
         e.preventDefault(); //submit 기능 중지 - 전송 중지
 
     };
-
+    /*이벤트가 발생하면 이라는 의미 e -> event 발생시 를 의미*/
     return(
         <div>
             <form className="enrolForm" onSubmit={handleSubmit}>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from './App.js';
+//App 컴퍼넌트 가져오기import 선언문
+import App from './App.js';        //이렇게 가져오기 구문으로 지정하여 내보냈던 걸 가져올 수 있다.
 
+// ReactDOM 라이브러리의 RENDER함수를 이용해서 의해 index.html의 태그들 중 id 가 app인 요소에 
+// 해당 컴퍼넌트를 표시함
+// render (컴퍼넌트 , 표시 위치)
 ReactDOM.render(
     <React.StrictMode>
         <h1>Just React</h1>
@@ -9,7 +13,7 @@ ReactDOM.render(
     document.querySelector('#app')
 );
 
-
+//App 이라는 컴퍼넌트를 #root에 표시 
 ReactDOM.render(
     <React.StrictMode>
         <App />
